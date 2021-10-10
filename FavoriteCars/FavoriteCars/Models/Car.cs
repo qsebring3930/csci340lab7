52338826891f915e3ea2424ead461bd48aaf2588
Thomas <quentin.sebring@icloud.com>
2021-10-09 21:19:17 -0500

    added car make filter

--- FavoriteCars/FavoriteCars/Models/Car.cs
+++ FavoriteCars/FavoriteCars/Models/Car.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace FavoriteCars.Models
 {
     public class Car
     {
         public int ID { get; set; }
+
         public int Year { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Make { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Model { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string Trim { get; set; }
     }
 }
--- FavoriteCars/FavoriteCars/Pages/Cars/Index.cshtml.cs
+++ FavoriteCars/FavoriteCars/Pages/Cars/Index.cshtml.cs
@@ -25,16 +25,26 @@ namespace FavoriteCars.Pages.Cars
         public string SearchString { get; set; }
         public SelectList Makes { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string CarMakes { get; set; }
+        public string CarMake { get; set; }
 
         public async Task OnGetAsync()
         {
+            IQueryable<string> makeQuery = from c in _context.Car
+                                            orderby c.Make
+                                            select c.Make;
+
             var cars = from c in _context.Car
                          select c;
             if (!string.IsNullOrEmpty(SearchString))
             {
                 cars = cars.Where(s => s.Model.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(CarMake))
+            {
+                cars = cars.Where(x => x.Make == CarMake);
+            }
+            Makes = new SelectList(await makeQuery.Distinct().ToListAsync());
 
             Car = await cars.ToListAsync();
         }
