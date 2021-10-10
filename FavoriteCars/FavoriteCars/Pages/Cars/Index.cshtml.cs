53b9fd1287b76398acc2abc56ff1e9262955dd44
Thomas <quentin.sebring@icloud.com>
2021-10-09 21:04:59 -0500

    added search bar

--- FavoriteCars/FavoriteCars/Pages/Cars/Index.cshtml.cs
+++ FavoriteCars/FavoriteCars/Pages/Cars/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FavoriteCars.Data;
 using FavoriteCars.Models;
@@ -20,10 +21,22 @@ namespace FavoriteCars.Pages.Cars
         }
 
         public IList<Car> Car { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Makes { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CarMakes { get; set; }
 
         public async Task OnGetAsync()
         {
-            Car = await _context.Car.ToListAsync();
+            var cars = from c in _context.Car
+                         select c;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                cars = cars.Where(s => s.Model.Contains(SearchString));
+            }
+
+            Car = await cars.ToListAsync();
         }
     }
 }
