81c2c3963d153c0c08831dd96a9ea2f6400595a4
Thomas <quentin.sebring@icloud.com>
2021-10-09 19:23:38 -0500

    Model Created

---
+++ FavoriteCars/FavoriteCars/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FavoriteCars.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Trim { get; set; }
+    }
+}
+
